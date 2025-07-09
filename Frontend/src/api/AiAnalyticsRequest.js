73db2fc71db52974380af10d936b56b7d7a8fffd
hafizahteshamali <hafizahteshamali363617@gmail.com>
2025-07-09 15:19:15 +0500

    ab hogaya

--- Frontend/src/api/AiAnalyticsRequest.js
+++ Frontend/src/api/AiAnalyticsRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify"; // Optional if you want to show toast here directly
 
 const AiAnalyticsApi = axios.create({
-  baseURL: "https://0abdd0dedc26.ngrok-free.app",
+  baseURL: "https://e0a43e522c53.ngrok-free.app",
   timeout: 30000,
   headers: {
     "Content-Type": "multipart/form-data",
