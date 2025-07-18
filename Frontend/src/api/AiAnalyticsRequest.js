93c0dc216dded29dac4231ceb5861a71dc939fd4
hafizahteshamali <hafizahteshamali363617@gmail.com>
2025-07-18 19:39:47 +0500

    changeeeee

--- Frontend/src/api/AiAnalyticsRequest.js
+++ Frontend/src/api/AiAnalyticsRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify"; // Optional if you want to show toast here directly
 
 const AiAnalyticsApi = axios.create({
-  baseURL: "http://37.120.176.43",
+  baseURL: "https://37.120.176.43",
   timeout: 30000,
   headers: {
     "Content-Type": "multipart/form-data",
